b16dfbddd9a45e712ab3ae9a7bbbc6c2ce4172c4
jansmi <janek.smielowski@gmail.com>
2020-10-27 08:46:22 +0100

    Cleanup

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -16,18 +16,29 @@ struct Task {
     int weight;
 };
 
-int getWeights(int time, Task left, Task right) {
-    if (time < left.readinessTime) {
-        time = left.readinessTime;
+class TimeWeight {
+public:
+    int time;
+    int weight;
+    
+    bool operator< (TimeWeight right) {
+        return this->weight < right.weight;
     }
-    time += left.processingTime;
-    int t1Weight = time > left.deadlineTime ? left.weight : 0;
-    if (time < right.readinessTime) {
-        time = right.readinessTime;
+};
+
+TimeWeight getTimeWeight(int time, Task task) {
+    if (time < task.readinessTime) {
+        time = task.readinessTime;
     }
-    time += right.processingTime;
-    int t2Weight = time > right.deadlineTime ? right.weight : 0;
-    return t1Weight + t2Weight;
+    time += task.processingTime;
+    int weight = time > task.deadlineTime ? task.weight : 0;
+    return {time, weight};
+}
+
+TimeWeight getTimeWeights(int time, Task left, Task right) {
+    TimeWeight leftTW = getTimeWeight(time, left);
+    TimeWeight rightTW = getTimeWeight(leftTW.time, right);
+    return {rightTW.time, leftTW.weight + rightTW.weight};
 }
 
 int main(int argc, char** argv) {
@@ -69,16 +80,18 @@ int main(int argc, char** argv) {
             for (int k = j + 1; k < INSTANCE_SIZES[i]; ++k) {
                 Task left = instances[i][j], right = instances[i][k];
 
-                int beforeSwapWeight = getWeights(time, left, right);
-                int afterSwapWeight = getWeights(time, right, left);
+                TimeWeight beforeSwap = getTimeWeights(time, left, right);
+                TimeWeight afterSwap = getTimeWeights(time, right, left);
 
-                if (afterSwapWeight < beforeSwapWeight) {
+                if (afterSwap < beforeSwap) {
                     swap(instances[i][j], instances[i][k]);
                 }
             }
 
-            globalTime = (globalTime < instances[i][j].readinessTime ? instances[i][j].readinessTime : time) + instances[i][j].processingTime;
-            globalCriterion += time > instances[i][j].deadlineTime ? instances[i][j].weight : 0;
+            Task firstTask = instances[i][j];
+            TimeWeight globals = getTimeWeight(globalTime, firstTask);
+            globalTime = globals.time;
+            globalCriterion += globals.weight;
         }
 
         cout << INSTANCE_SIZES[i] << ": t=" << globalTime << ", c=" << globalCriterion << endl;
