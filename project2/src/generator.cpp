ea914ff1438226f63741bde448ff88efad6eeaed
jansmi <janek.smielowski@gmail.com>
2020-11-15 14:02:44 +0100

    Fixed generator

--- project2/src/generator.cpp
+++ project2/src/generator.cpp
@@ -9,26 +9,49 @@ float randomFloat(float a, float b) {
     return a + r;
 }
 
-void generate(string path, int instanceSize) {
-    string filePath = path + "\\in_136815_" + to_string(instanceSize) + ".txt";
+vector<Task> generateInstance(int instanceSize) {
+    vector<Task> instance;
+    
+    for (int i = 0; i < instanceSize; ++i) {
+        Task task;
+        task.duration = rand() % GEN_TASK_MAX_DURATION + 1;
+        task.readiness = rand() % instanceSize;
+        instance.push_back(task);
+    }
+
+    return instance;
+}
+
+vector<float> generateMachines() {
+    vector<float> machines;
+
+    for (int m = 0; m < MACHINES - 1; ++m) {
+        machines.push_back(randomFloat(1, GEN_MACHINE_FACTOR_RANGE));
+    }
+
+    machines.push_back(1.f);
+    random_shuffle(machines.begin(), machines.end());
+
+    return machines;
+}
+
+void generate(int instanceSize) {
+    string filePath = PATH + INDEX_INPATH + "_" + to_string(instanceSize) + ".txt";
     ofstream outfile(filePath);
+
+    vector<float> machines = generateMachines();
+    vector<Task> instance = generateInstance(instanceSize);
 
     outfile << instanceSize << endl;
 
-    for (int m = 0; m < 5; ++m) {
-        float machineSpeedFactor = randomFloat(1, GEN_MACHINE_RANGE);
-        outfile << fixed << setprecision(GEN_MACHINE_PRECISION) << machineSpeedFactor;
-        if (m < 4) {
-            outfile << " ";
-        }
+    for (int m = 0; m < MACHINES - 1; ++m) {
+        outfile << fixed << setprecision(GEN_MACHINE_PRECISION) << machines[m] << " ";
     }
 
-    outfile << endl;
+    outfile << fixed << setprecision(GEN_MACHINE_PRECISION) << machines[MACHINES - 1] << endl;
 
     for (int i = 0; i < instanceSize; ++i) {
-        int taskDuration = rand() % GEN_TASK_MAX_DURATION + 1;
-        int taskReadiness = rand() % instanceSize;
-        outfile << taskDuration << " " << taskReadiness << endl;
+        outfile << instance[i].duration << " " << instance[i].readiness << endl;
     }
 
     outfile.close();
@@ -38,14 +61,11 @@ int main() {
     srand(time(NULL));
     rand();
 
-    const string INSTANCES_PATH = ".\\instances";
-    const string INDEX_PATH = "\\in136815";
-    const string PATH = INSTANCES_PATH + INDEX_PATH;
     CreateDirectoryA(INSTANCES_PATH.c_str(), NULL);
     CreateDirectoryA(PATH.c_str(), NULL);
 
     for (int i = 0; i < INSTANCES; ++i) {
-        generate(PATH, INSTANCE_SIZES[i]);
+        generate(INSTANCE_SIZES[i]);
     }
 
     return EXIT_SUCCESS;
--- project2/src/settings.h
+++ project2/src/settings.h
@@ -1,19 +1,38 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <iomanip>
-#include <iostream>
+#include <algorithm>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
 #include <vector>
 #include <windows.h>
 
 const int INSTANCES = 10;
 const int INSTANCE_SIZES[] = { 50, 100, 150, 200, 250, 300, 350, 400, 450, 500 };
+const int MACHINES = 5;
+
+const std::string INSTANCES_PATH = ".\\instances";
+const std::string INDEX_INPATH = "\\in_136815";
+const std::string INDEX_OUTPATH = "\\out_136815";
+const std::string PATH = INSTANCES_PATH + INDEX_INPATH;
 
 const int GEN_MACHINE_PRECISION = 2;
-const int GEN_MACHINE_RANGE = 5;
+const int GEN_MACHINE_FACTOR_RANGE = 5;
 const int GEN_TASK_MAX_DURATION = 100;
+
+struct Machine {
+    int factor;
+    int time;
+    std::vector<int> schedule;
+};
+
+struct Task {
+    int duration;
+    int readiness;
+};
 
 #endif
